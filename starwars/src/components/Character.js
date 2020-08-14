0848867f0da9ae60128a762d1d2ced9af4a13352
JRIGS <raczkowskijonathan@gmail.com>
2020-08-14 13:34:16 -0400

    Added full-functionality/Still needs styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react';
+
+
+function Character(props){
+const {passingCharacterState} = props
+
+return (
+
+<div>
+    
+    <h2>Name: {passingCharacterState.name}</h2>
+    <h3>Birthyear: {passingCharacterState.birth_year}</h3>
+    <p>Gender: {passingCharacterState.gender}</p>
+    <p>Height: {passingCharacterState.height} Inches</p>
+    <p>Weight: {passingCharacterState.mass} Kilograms</p>
+    <p>Eye Color: {passingCharacterState.eye_color}</p>
+    <p>Hair Color: {passingCharacterState.hair_color}</p>
+
+</div>
+
+)
+
+}
+
+export default Character
